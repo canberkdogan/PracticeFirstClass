d0a0cc6e6a8e859fdf3ccb6387d8b1402569050e
canbe <canbe@CANBERK>
2024-08-06 23:56:06 +0300

    Add project files.

---
+++ PracticeFirstClass/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeFirstClass
+{
+    public class Person
+    {
+        public string Name {  get; set; }
+
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public void DisplayInfo () 
+        {
+            Console.WriteLine($"Ad {Name}, Soyadı {Surname}, Doğum Tarihi {BirthDate.ToShortDateString()}");
+
+        }
+    }
+}
---
+++ PracticeFirstClass/Program.cs
@@ -0,0 +1,56 @@
+﻿using PracticeFirstClass;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Person ogrenci = new Person();
+        
+        ogrenci.Name = "Canberk";
+
+        ogrenci.Surname = "Doğan";
+
+        ogrenci.BirthDate = new DateTime(1999, 06, 21);
+
+
+        Person ogrenci1 = new Person();
+
+        ogrenci1.Name = "Ceyda";
+
+        ogrenci1.Surname = "Yılmaz";
+
+        ogrenci1.BirthDate = new DateTime(1999, 02, 07);
+
+
+
+        Person ogretmen = new Person();
+
+        ogretmen.Name = "Carl";
+
+        ogretmen.Surname = "Johnson";
+
+        ogretmen.BirthDate = new DateTime(1986, 10, 06);  
+
+
+        Person ogretmen1 = new Person();
+
+        ogretmen1.Name = "Walter";
+
+        ogretmen1.Surname = "White";
+
+        ogretmen1.BirthDate = new DateTime(1970, 05, 27);
+
+
+        Console.WriteLine("Students");
+        ogrenci.DisplayInfo();
+        ogrenci1.DisplayInfo();
+
+        Console.WriteLine("Teachers");
+        ogretmen.DisplayInfo();
+        ogretmen1.DisplayInfo();
+
+
+
+    }
+
+}
